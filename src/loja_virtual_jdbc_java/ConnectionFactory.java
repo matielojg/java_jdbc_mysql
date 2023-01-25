01e20e0847dc2087e8b460fce3b4d9b29ec0cd68
Matielo J Geronimo <matielo.dev@gmail.com>
2023-01-25 10:47:24 -0300

    Criação connectionFactory

---
+++ src/loja_virtual_jdbc_java/ConnectionFactory.java
@@ -0,0 +1,16 @@
+package loja_virtual_jdbc_java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	public Connection recuperarConexao() throws SQLException {
+
+		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
+				"root", "");
+
+	}
+
+}
