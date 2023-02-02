bb76a89cf93272260b20b8e70bd2342a04da7ff8
Matielo J Geronimo <matielo.dev@gmail.com>
2023-02-02 16:37:10 -0300

    Listando com InnerJoin + Performatico

--- src/loja_virtual_jdbc_java/modelo/Categoria.java
+++ src/loja_virtual_jdbc_java/modelo/Categoria.java
@@ -1,8 +1,13 @@
 package loja_virtual_jdbc_java.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
+
 	private Integer id;
 	private String nome;
+	private List<Produto> produtos = new ArrayList<Produto>();
 
 	public Categoria(int id, String nome) {
 		this.id = id;
@@ -15,5 +20,13 @@ public class Categoria {
 
 	public int getId() {
 		return id;
+	}
+
+	public void adicionar(Produto produto) {
+		produtos.add(produto);
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
 	}
 }
